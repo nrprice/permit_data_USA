d25a9901e0d8391f161d9a95998d7edffb97c500
acrobaticenglishman <63313551+acrobaticenglishman@users.noreply.github.com>
2021-04-01 19:58:29 +0100

    New Data + Cleaning

--- data_import.py
+++ data_import.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from datetime import datetime as dt
 pd.set_option("display.max_columns", 50)
 pd.set_option("display.max_rows", 400000)
@@ -7,23 +8,40 @@ pd.set_option("display.width", 1000)
 # Inital Import
 gun_data = pd.read_csv('Assets/input_nics_updated.csv')
 election_data = pd.read_csv('Assets/input_presidents.csv')
-census = pd.read_csv('Assets/input_census_clean.csv')
+census = pd.read_excel('Assets/input_census.xlsx', engine='openpyxl')
 
+states_list = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+       'Colorado', 'Connecticut', 'Delaware', 'District of Columbia',
+       'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana',
+       'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+       'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+       'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+       'New Jersey', 'New Mexico', 'New York', 'North Carolina',
+       'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+       'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+       'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+       'West Virginia', 'Wisconsin', 'Wyoming', 'Puerto Rico']
 
 # Census Data
-census.drop(columns=['Unnamed: 0', 'census', 'estimates_base'],  inplace=True)
-census.rename(columns={'location': 'state'}, inplace=True)
-print (census['state'].unique())
+new_headers = census.iloc[2]
+census = census[3:].reset_index(drop=True)
+census.columns = new_headers
+census = census.rename_axis(None, axis=1)
+census.rename(columns={np.nan: 'state'}, inplace=True)
+census.columns = [x.lower().replace(" ", "_") if type(x) == str else int(x) for x in census.columns]
+census['state'] = census['state'].apply(lambda x: str(x).replace(".", ""))
+census.drop(['census', 'estimates_base'], axis=1, inplace=True)
+census = census[census['state'].isin(states_list)]
+
 # Census Data Melted
 census_melted = census.melt(id_vars='state', var_name='date', value_name='count')
-census_melted['date'] = pd.to_datetime(census_melted['date'])
+census_melted['date'] = pd.to_datetime(census_melted['date'], format='%Y')
 census_melted = census_melted[['date', 'state', 'count']]
 
 # Gun Data
 gun_data.rename(columns={'month': 'date'}, inplace=True)
 gun_data = gun_data[['date', 'state', 'permit', 'handgun', 'long_gun', 'multiple']]
 gun_data['date'] = pd.to_datetime(gun_data['date'])
-
 
 # Election Data
 election_data.columns = [x.lower().replace(' ', '_') for x in election_data.columns]
@@ -49,7 +67,6 @@ for state in full_states_list:
         winning_party_df = winning_party_df.append(most_votes_cast, ignore_index=True)
 
 winning_party_df['date'] = winning_party_df['date'].apply(lambda x: dt(x, 1, 1))
-
 
 # Census and Winning Party Combined
 census_and_party_df = census_melted.merge(winning_party_df,
